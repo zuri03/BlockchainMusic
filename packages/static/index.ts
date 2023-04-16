03d4239126b6051771f203d06b5c28a47f06b679
zuri03 <zls6563@g.rit.edu>
2023-04-16 00:25:26 -0400

    set up docker

--- packages/static/index.ts
+++ packages/static/index.ts
@@ -1,1 +1,9 @@
-console.log('hello world')
+import express from 'express'
+
+const app : express.Application = express()
+
+app.get('/', function (req : express.Request, res : express.Response) : void {
+    res.send("SUCCESS!!!")
+})  
+
+app.listen(8000);
