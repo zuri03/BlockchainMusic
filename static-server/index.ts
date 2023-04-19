99c123784292dbafad50dec11879ca3920cb69ed
zuri03 <zls6563@g.rit.edu>
2023-04-18 20:51:02 -0400

    set up static server with docker

--- static-server/index.ts
+++ static-server/index.ts
@@ -1,9 +1,18 @@
 import express from 'express'
+import path from 'path'
 
+const PORT = 8000;
 const app : express.Application = express()
 
+/*
 app.get('/', function (req : express.Request, res : express.Response) : void {
+    console.log(process.cwd())
+    console.log(path.join(process.cwd(), 'public'))
     res.send("SUCCESS!!!")
-})  
+}); 
+*/
 
-app.listen(8000);
+//app.use(express.static('public'));
+app.use(express.static(path.join(process.cwd(), 'public')))
+
+app.listen(PORT, () => console.log(`server listening on port: ${PORT}`));
