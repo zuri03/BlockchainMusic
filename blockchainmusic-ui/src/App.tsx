4030b4cc1949eca2f15cd6f6f745c0240dd57dc1
zuri03 <zls6563@g.rit.edu>
2023-04-19 15:06:26 -0400

    testing react ui

---
+++ blockchainmusic-ui/src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AppHeader from './components/appHeader';
+
+function App() {
+  return (
+    <div className="App">
+      <AppHeader />
+    </div>
+  )
+  /*
+  return (
+    <div className="App">
+      
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          
+        </a>
+      </header>
+    </div>
+  );
+  */
+}
+
+export default App;
