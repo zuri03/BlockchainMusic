3458808844949bc1a4f6e8074a69638f672463be
zuri03 <zls6563@g.rit.edu>
2023-04-18 21:37:11 -0400

    updated todo

--- static-server/index.ts
+++ static-server/index.ts
@@ -4,15 +4,6 @@ import path from 'path'
 const PORT = 8000;
 const app : express.Application = express()
 
-/*
-app.get('/', function (req : express.Request, res : express.Response) : void {
-    console.log(process.cwd())
-    console.log(path.join(process.cwd(), 'public'))
-    res.send("SUCCESS!!!")
-}); 
-*/
-
-//app.use(express.static('public'));
 app.use(express.static(path.join(process.cwd(), 'public')))
 
 app.listen(PORT, () => console.log(`server listening on port: ${PORT}`));
