f3829f8445743d4497a4fd8ccc98d753fcce8fc3
zuri03 <zls6563@g.rit.edu>
2023-05-08 12:26:32 -0400

    Added temp music repo

---
+++ music-service/db/song-repository.ts
@@ -0,0 +1,66 @@
+//For now the db will remain as internal memory until an external db is added
+type Song = {
+    id: string,
+    title: string,
+    author: string,
+    authorId: string,
+    description: string,
+    createdAt: string 
+}
+
+export default class SongRepository {
+
+    private songs: Song[];
+
+    public constructor() {
+        this.songs =  [{
+            id: '1',
+            title: 'title',
+            authorId: 'authorId',
+            author: 'author',
+            createdAt: 'createdAt',
+            description: 'description'
+        },
+        {
+            id: '2',
+            title: 'title',
+            authorId: 'authorId',
+            author: 'author',
+            createdAt: 'createdAt',
+            description: 'description'
+        },
+        {
+            id: '3',
+            title: 'title',
+            authorId: 'authorId',
+            author: 'author',
+            createdAt: 'createdAt',
+            description: 'description'
+        }];
+    }
+
+    public GetAllSongs() : Song[] {
+        return this.songs;
+    } 
+
+    public GetSong(id: number | string) : Song | undefined {
+        id = id.toString();
+        return this.songs.find(song => song['id'] === id);
+    }
+
+    public AddSong(newSong : Song): void {
+        this.songs.push(newSong);
+    }
+
+    public DeleteSong(id: number | string) : void {
+        id = id.toString();
+        const deletedElementIndex = this.songs.findIndex(song => song['id'] === id);
+
+        if (deletedElementIndex === -1) {
+            throw new Error(`No song with id ${id} found`);
+        }
+
+        this.songs = this.songs.splice(deletedElementIndex, 1);
+    }
+
+} 
