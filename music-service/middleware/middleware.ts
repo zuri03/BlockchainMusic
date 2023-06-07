e25b0b12dd0e6fed9ab38c848b8e3887047b8da0
zuri03 <zls6563@g.rit.edu>
2023-06-07 18:54:24 -0400

    updated todo

--- music-service/middleware/middleware.ts
+++ music-service/middleware/middleware.ts
@@ -9,17 +9,3 @@ export const CustomErrorHandler = function (error: any, request: express.Request
 
     response.status(error.statusCode).json({ 'result': 'error handled' })
 }   
-
-export const SetAccessControlHeaders = function (error: any, request: express.Request, response: express.Response, next: express.NextFunction) {
-    const headers = {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Headers" : "Content-Type",
-        "Access-Control-Allow-Origin": "https://zls6563.gitlab.io",
-        "Access-Control-Allow-Methods": 'OPTIONS,POST,GET,PATCH'
-    };
-
-    response.setHeader("Content-Type", "application/json");
-    response.setHeader("Access-Control-Allow-Headers",  "Content-Type");
-    response.setHeader("Access-Control-Allow-Origin",  "*");
-    response.setHeader("Access-Control-Allow-Methods",  'OPTIONS,POST,GET,PATCH,DELETE');
-}
