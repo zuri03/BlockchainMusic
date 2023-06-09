d3ead0dee061f7915ae90dad02f7fde02d54a243
zuri03 <zls6563@g.rit.edu>
2023-06-09 16:43:10 -0400

    Set up gracefule shutdown

--- music-service/index.ts
+++ music-service/index.ts
@@ -4,28 +4,15 @@ const app = setUp();
 
 //temp port
 const PORT = 8888;
-app.listen(PORT, () => console.log(`server listening on port: ${PORT}`));
+const server = app.listen(PORT, () => console.log(`server listening on port: ${PORT}`));
 
-/*
-GET: /Song: Gets info on all music available (will need pagination),
-GET: /Song/<id>: Gets information for a specific song
-GET: /Song/Search/<searchTerm>: Gets all music where the title matches the serach term
-POST: /Song: Adds new music to the DB
-{
-    'id': <id>
-    'title': 'example', //song title
-    'author': 'example', //username of the user who uploaded the song
-    'authorId' <id>, //id of the user who uploaded the song
-    'cover': <coverId>, //id of the cover image for the song (may not be included)
-    'createdAt': <datetime>, //datetime created on the server
+const gracefulServerShutdown = function (signal: string) {
+    console.log(`${signal} recieved, shutting down server`);
+    server.close(() => {
+        console.log('Server exiting...')
+    });
 }
-DELETE: /Music/<id>: Deletes music from the db
 
-The author routes may not be necessary
-GET: /Author: Gets informaiton on all authors
-GET: /Author/<id>: Gets information on a specific artist
-GET: /Author/Search/<searchTerm>: Gets info on all artists that match a search term
-GET: /Cover/<id>: Gets a song's cover image 
-
-*/
+process.on('SIGTERM', () => gracefulServerShutdown('SIGTERM'));
+process.on('SIGINT', () => gracefulServerShutdown('SIGINT'));
  
