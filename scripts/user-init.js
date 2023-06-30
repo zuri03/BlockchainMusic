8fed30780ccfbeb2b1a90522af446544ed0efb20
zuri03 <zls6563@g.rit.edu>
2023-06-29 20:28:44 -0400

    removed console.logs from init scripts

--- scripts/user-init.js
+++ scripts/user-init.js
@@ -1,7 +1,5 @@
-console.log('MONGO INIT SCRIPT STARTED!')
 db.createUser({
     user: encodeURIComponent(process.env.MONGO_USER_SERVICE_USERNAME),
     pwd: encodeURIComponent(process.env.MONGO_USER_SERVICE_PASSWORD),
     roles : [{ role: 'readWrite', db: 'users' }]
 });
-console.log('MONGO INIT SCRIPT COMPLETE!')
