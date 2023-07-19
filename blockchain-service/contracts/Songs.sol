f543347a6caffb97d41b3cec0b4399f3f9b2069d
zuri03 <zls6563@g.rit.edu>
2023-07-19 17:33:11 -0400

    added function to contract

--- blockchain-service/contracts/Songs.sol
+++ blockchain-service/contracts/Songs.sol
@@ -2,15 +2,28 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Songs {
-    //owner: address
-
-    //map songid: string => song data: bytes
+    //the artist who will save their songs to this contract
+    address payable private owner;
+    
+    //mapping songid => song data
+    mapping (address => bytes) private songRepository;
 
     //constructor
+    constructor () {
+        owner = payable(msg.sender);
+    }
 
     //getSongData(songid: string) returns songdata: bytes
+    function getSongData (string calldata songid) public view returns(bytes memory data) {
+
+    }
 
     //uploadSongData(songid: string, songdata: bytes) returns nothing
+    function uploadSongData (string calldata songid, string calldata songData) public {
 
+    }
     //deleteSong(songid: string) returns nothing
+    function deleteSong (string calldata songid) public {
+        
+    }
 }
