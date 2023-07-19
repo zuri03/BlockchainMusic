fbd18185b52da581df9d54f7a2033d803c018f8e
zuri03 <zls6563@g.rit.edu>
2023-07-19 15:50:06 -0400

    added skeleton of contract

---
+++ blockchain-service/contracts/Songs.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Songs {
+    //owner: address
+
+    //map songid: string => song data: bytes
+
+    //constructor
+
+    //getSongData(songid: string) returns songdata: bytes
+
+    //uploadSongData(songid: string, songdata: bytes) returns nothing
+
+    //deleteSong(songid: string) returns nothing
+}
